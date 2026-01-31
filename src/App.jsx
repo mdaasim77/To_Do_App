8b335a203aaec5678f5ba34dd446515abb2412e9
mdaasim77 <khanaasim9910@gmail.com>
2026-01-31 14:51:08 +0530

    error in multiplying cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,27 @@ import Section2 from "./UI Project/Section2/Section2";
 import "remixicon/fonts/remixicon.css";
 
 export default function App() {
+  const user = [
+    {
+      img: "girl.jpg",
+      para: "lorem13 wjddncw ubc dfi aideu  uie dqiu duif",
+      btn: "Designer",
+    },
+    {
+      img: "man2.jpg",
+      para: "lorem13 wjddncw ubc dfi aideu  uie dqiu duif",
+      btn: "Manager",
+    },
+    {
+      img: "man3.jpg",
+      para: "lorem13 wjddncw ubc dfi aideu  uie dqiu duif",
+      btn: "Programer",
+    },
+  ];
   return (
     <div>
-      <Section1/>
-      <Section2/>
+      <Section1 />
+      <Section2 />
       {/* <Parent /> */}
     </div>
   );
